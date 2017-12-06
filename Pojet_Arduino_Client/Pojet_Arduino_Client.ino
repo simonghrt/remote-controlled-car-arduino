17b1940e41c1b51a7d52a19c7bafc00eba0e1000
Simon Gheeraert <simon.gheeraert.sg@gmail.com>
2017-12-06 23:58:23 +0100

    Code cleaned

--- Pojet_Arduino_Client/Pojet_Arduino_Client.ino
+++ Pojet_Arduino_Client/Pojet_Arduino_Client.ino
@@ -8,7 +8,6 @@ int calX, calY, valX, valY, val_potar;
 const int VRx = 0; // A0
 const int VRy = 1; // A1
 const int A_potar = 3; //A3
-const int D_servo = 2;
 const int A_RED_LED = 11; //A11
 const int A_YELLOW_LED = 13; //A13
 
@@ -26,19 +25,9 @@ void setup() {
 
   calX = analogRead(VRx); 
   calY = analogRead(VRy);
-
-  myservo.attach(D_servo);
-  
 }
 
 void loop() {
-  
-  /*BridgeClient client;// Construct the client
-  if(client.connect(ip_server,5555)) //return 1 if connection established ; 0 else
-      client.write(42);//return 1 if sent ; 0 else
-    //client.write(const uint8_t *buf, size_t size)//écrit buf dans le buffer de transmision
-  
-  client.stop();*/
 
   // Mesure des valeurs en X et Y
   valX = analogRead(VRx)- calX; // -255 to 255
@@ -49,24 +38,27 @@ void loop() {
   int vitG, vitD; // -255 to 255 (à la fin des calculs)
   int dir; // 0 : avance; 1 : recule
 
+  // Détermination des directions du robot
   if (valX == 0) {
     dir = 0;
   } else if (valX > 0) {
     dir = 0;
   } else {
     dir = 1;
+    // Si on a X négatif, on prend la valeur absolue par la suite puisqu'on a en direction 1
     valX = -valX;  
   }
 
-
-
+  // Si valeurs très faibles, on considère que c'est nul
   if (valX < 4) {
     vitG = 0;
     vitD = 0;
   } else {
+    // Calcul des vitesses en utilisant X et Y
     vitG = valX + (255 - max(0, valY));
     vitD = valX + (255 - max(0, -valY));
-    
+
+    // On prend en compte la valeur du potar
     vitG = vitG + (val_potar/2);
     vitD = vitD + (val_potar/2);
   
@@ -83,60 +75,11 @@ void loop() {
   analogWrite(A_RED_LED,vitG);
   analogWrite(A_YELLOW_LED,vitD);
 
-  // String str = vitG + ',' + vitD + ',' + dir + "\n";
-
   String str = String(vitG) + String(",") + String(vitD) + String(",") + String(dir);
 
   Serial.println(str);
   
   delay(100);
-
-  
-//  if (valX == 0) {
-//    vitG = 0;
-//    vitD = 0;
-//  } else if (valX > 0) {
-//    vitG = valX + (255 - max(0, valY));
-//    vitD = valX + (255 - max(0, -valY));
-//  } else {
-//    vitG = valX - (255 - max(0, valY));
-//    vitD = valX - (255 - max(0, -valY));  
-//  }
-
-//  if (vitG == 0 && vitD == 0) {
-//    val_potar = 0;
-//  }
-//
-//  if (vitG > 0) {
-//    vitG = vitG + (val_potar/2);
-//    vitD = vitD + (val_potar/2);
-//  } else if (vitG < 0) {
-//    vitG = vitG - (val_potar/2);
-//    vitD = vitD - (val_potar/2);
-//  }
-//
-//  vitG = vitG / 4;
-//  vitD = vitD / 4;
-
-  // Affichage des valeurs lues
-//  Serial.print("--- Nouvelles lectures --- ");
-//  Serial.println("Valeur X : ");
-//  Serial.println((int)(valX));
-//  Serial.print("Valeur y : ");
-//  Serial.println((int)(valY));
-//  Serial.print("Valeur Potar : ");
-//  Serial.println((int)(val_potar));
-//  Serial.print("Valeur Gauche : ");
-//  Serial.println((int)(vitG));
-//  Serial.print("Valeur Droite : ");
-//  Serial.println((int)(vitD));
-
-  
-  // myservo.writeMicroseconds(10*valX);
-
-  //analogWrite(A_RED_LED,val_potar/4);
-  //analogWrite(A_YELLOW_LED,val_potar/4);
-
 }
 
 
